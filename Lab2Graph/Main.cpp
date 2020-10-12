95fcc893e0466ead24188645349c7e034b095091
Сафиюлин Александр <rootalfa4@gmail.com>
2020-10-12 19:26:20 +0300

    Updateted main. Now it paints three triangles. Hooray!

--- Lab2Graph/Main.cpp
+++ Lab2Graph/Main.cpp
@@ -1,14 +1,28 @@
 #include <cmath>                                        // для функции sin
 #include <Windows.h>
-#include <iostream>                                     // для стандартных потоков ввода-вывода
-#include <numbers>
 
+#include "RenderSystem.h"
 #include "Window.h"
 
 using namespace std;
 
+template <class T>
+auto Println(const T i, ostream& out = cout) -> void {
+	out << i << endl;
+}
+
 auto Hello() -> void {
 	std::cout << "Lab 2" << std::endl << "Author: Alex Safiyulin" << std::endl;
+}
+
+auto MakeTriangle(GLFWwindow* window, chupakabra::render::IRenderSystem* renderer) -> void {
+	glfwMakeContextCurrent(window);
+	renderer->Render(window);
+	glfwSwapBuffers(window);
+}
+
+auto MakeTriangle(chupakabra::graphics::Window& window, chupakabra::render::IRenderSystem* renderer) -> void {
+	MakeTriangle(window.GetWindow(), renderer);
 }
 
 auto main() -> int {
@@ -18,28 +32,46 @@ auto main() -> int {
 
 	Hello();
 
-	auto* window = chupakabra::graphics::Initializer::InitializeWindow(4.6, 400u);
-	
+	auto* system = new chupakabra::render::OldRenderSystem();
+	system->Initialize();
+
+	chupakabra::graphics::Window w1(300u, "W1"), w2(300u, "W2");
+	auto* window = glfwCreateWindow(300, 300, "Window", nullptr, nullptr);
+
 	if (!window) {
 		return EXIT_FAILURE;
 	}
 
+	glfwMakeContextCurrent(window);
+
+	// Инициализируем GLEW
+	glewExperimental = true; // Флаг необходим в Core-режиме OpenGL
+	if (glewInit() != GLEW_OK) {
+		Println("Невозможно инициализировать GLEW", std::cerr);
+		return EXIT_FAILURE;
+	}
+
+	// Включим режим отслеживания нажатия клавиш, для проверки ниже
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
 	auto colorRgb = 0.f;
 	
-	while (!glfwWindowShouldClose(window->GetWindow())) {
-		glClearColor(static_cast<GLclampf>(cos(colorRgb * std::numbers::pi / 90.f)), static_cast<GLclampf>(abs(sin(colorRgb * std::numbers::pi / 90.f))),
-			static_cast<GLclampf>(sin(colorRgb * std::numbers::pi / 90.f)) + static_cast<GLclampf>(cos(colorRgb * std::numbers::pi / 90.f)),static_cast<GLclampf>(1.f));
+	while (!glfwWindowShouldClose(window)) {
+		while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0) {
 
-		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(window->GetWindow());
-		glfwPollEvents();
+			MakeTriangle(w1, system);
+			MakeTriangle(w2, system);
+			MakeTriangle(window, system);
+
+			glfwPollEvents();
+			glfwMakeContextCurrent(window);
+		}
 
 		colorRgb <= 180.f ? colorRgb += 0.005f : colorRgb = 0.f;
 	}
 
-	delete window;
-
-	chupakabra::graphics::Deinitializer::Deinitialize();
+	delete system;
+	glfwTerminate();
 	
 	return EXIT_SUCCESS;
 }
